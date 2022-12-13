d4dc056df3739d2808820dd44bf0a8e356ffc0c5
Olena <newsroommy@gmail.com>
2022-12-13 11:06:17 +0200

    update fn onFormSubmit

--- src/index.js
+++ src/index.js
@@ -15,8 +15,64 @@ let items = [];
 
 function onFormSubmit(e) {
   e.preventDefault();
-  // значення input у формі
+  // значення input у формі по name
   const inputValue = e.target.elements.searchQuery.value.trim();
   console.log(inputValue);
-  fetchPictures(inputValue);
+  // if (!inputValue) {
+  //   return;
+  // }
+  fetchPictures(inputValue).then(res => {
+    console.log(res);
+  });
+  // if ( === []) {
+  //   Notify.failure(
+  //     'Sorry, there are no images matching your search query. Please try again.'
+  //   );
+  // }
 }
+
+// HOMEWORK 10
+// function onInput(e) {
+//   // дані з інпута
+//   const inputValue = e.target.value.trim();
+//   // очищаємо розмітку
+//   clearInput();
+//   // якщо пустий рядок false за допомогою інверсії змінюємо на true
+//   // тобто якщо пустий рядок - вийшли з функції
+//   if (!inputValue) {
+//     return;
+//   }
+//   // якщо не пустий рядок викликаємо функцію fetchСountries
+//   // передаємо значення яке ввів користувач inputValue
+//   fetchCountries(inputValue)
+//     // отримали дані response, прописуємо логіку, що робити з цими даними
+//     // т.ч. можемо перевикористовувати функцію
+//     .then(res => {
+//       console.log(res);
+//       const resLength = res.length;
+//       // якщо один об'єкт відмальовуємо картку однієї країни
+//       if (resLength === 1) {
+//         const markup = createOneCountryMarkup(res);
+//         refs.countryInfoRef.insertAdjacentHTML('beforeend', markup);
+//         return;
+//       }
+//       // якщо 2 об'єкти або 10 або менше 10 - малюємо список країн
+//       if (resLength > 2 && resLength <= 10) {
+//         const markup = createCountriesList(res);
+//         refs.listRef.insertAdjacentHTML('beforeend', markup);
+//         return;
+//       }
+//       // якщо більше 10 об'єктів(країн) виводити рядок
+//       if (resLength > 10) {
+//         Notify.info(
+//           'Too many matches found. Please enter a more specific name.'
+//         );
+//         return;
+//       }
+//     })
+//     .catch(error => {
+//       clearInput();
+//       console.log(error);
+//       Notify.failure('Oops, there is no country with that name');
+//     });
+// }
