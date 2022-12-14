abafb8506d142b211e16071f8ca9051364a64379
Olena <newsroommy@gmail.com>
2022-12-14 17:29:06 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ function onFormSubmit(e) {
   searchApiService
     .fetchSearchPictures()
     .then(hits => {
+      // очистка розмітки
       clearGalleryRef();
       appendPictureMarkup(hits);
     })
